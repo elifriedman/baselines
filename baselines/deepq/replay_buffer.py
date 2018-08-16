39cccdc63f8585d3782703df3663bc00f3869437
Eli Friedman <friedm3@cooper.edu>
2018-08-16 14:18:07 +0300

    test HER with deepq

--- baselines/deepq/replay_buffer.py
+++ baselines/deepq/replay_buffer.py
@@ -21,8 +21,7 @@ class ReplayBuffer(object):
     def __len__(self):
         return len(self._storage)
 
-    def add(self, obs_t, action, reward, obs_tp1, done):
-        data = (obs_t, action, reward, obs_tp1, done)
+    def add(self, data):
 
         if self._next_idx >= len(self._storage):
             self._storage.append(data)
@@ -32,15 +31,15 @@ class ReplayBuffer(object):
 
     def _encode_sample(self, idxes):
         obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
+        datas = tuple([] for i in self._storage[idxes[0]])
         for i in idxes:
             data = self._storage[i]
-            obs_t, action, reward, obs_tp1, done = data
-            obses_t.append(np.array(obs_t, copy=False))
-            actions.append(np.array(action, copy=False))
-            rewards.append(reward)
-            obses_tp1.append(np.array(obs_tp1, copy=False))
-            dones.append(done)
-        return np.array(obses_t), np.array(actions), np.array(rewards), np.array(obses_tp1), np.array(dones)
+            for j, d in enumerate(data):
+                if hasattr(d, "__len__"):
+                    datas[j].append(np.array(d, copy=False))
+                else:
+                    datas[j].append(d)
+        return tuple(np.array(d) for d in datas)
 
     def sample(self, batch_size):
         """Sample a batch of experiences.
